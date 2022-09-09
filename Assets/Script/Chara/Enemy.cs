f005b10c96c294bdb1bf35103e976b2944ac521b
tu8 <tanaka19175@gmail.com>
2022-09-09 16:44:25 +0900

    敵が被ダメでノックバックするように修正

--- Assets/Script/Chara/Enemy.cs
+++ Assets/Script/Chara/Enemy.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private enum State
+    {
+        Default,    // 静止or巡回
+        Move,       // プレイヤーに向かって動く
+        Attack,     // 攻撃
+        Knockback,    // ノックバック（被ダメ開始後１Fだけなる）
+        Damaging,     // 被ダメ
+    }
+
     [SerializeField]
     private Transform _imageTransform;
 
@@ -18,6 +27,7 @@ public class Enemy : MonoBehaviour
     private static readonly Vector3 UP = Vector3.up;
 
     private static readonly Vector3 GRAVITY_ACCELERATION = new Vector3(0f, -9.8f, 0f) * 80;
+    private static readonly Vector3 KNOCK_BACK_SPEED = new Vector3(300f, 0, 0);
     private Vector3 _previousPosition;
     private Vector3 _acceleration = Vector3.zero;
     private Vector3 _speed = Vector3.zero;
@@ -39,6 +49,8 @@ public class Enemy : MonoBehaviour
 
     private int _hp;
 
+    private State _state;
+
     /// <summary>
     /// 現在のモーションを続ける時間
     /// </summary>
@@ -59,7 +71,7 @@ public class Enemy : MonoBehaviour
         enemyManager._parameter = paramter;
         enemyManager.transform.localPosition = Vector3.zero;
         enemyManager._hp = paramter.HitPoint;
-
+        enemyManager._state = State.Default;
         if (paramter.DropItemId != 0)
         {
             // アイテムをドロップする敵は気持ちサイズを大きくする仮処理
@@ -80,6 +92,15 @@ public class Enemy : MonoBehaviour
 
     public void Update()
     {
+        if (_state == State.Knockback)
+        {
+            // ノックバック中は状態更新が何もできないようにする
+            _chase = false;
+            _moveLeft = false;
+            _moveRight = false;
+            return;
+        }
+
         if (_remainMoveTime > 0f)
         {
             // 現在のモーションを続ける場合は時間を更新
@@ -100,9 +121,13 @@ public class Enemy : MonoBehaviour
         _chase = false;
         _moveLeft = false;
         _moveRight = false;
+        _speed = ZERO;
+
+        // 次の状態を抽選
 
         if (_canAttack)
         {
+
             // 攻撃する
             Debug.Log("こうげき");
             return;
@@ -112,11 +137,13 @@ public class Enemy : MonoBehaviour
         var rand = Random.Range(0, 100);
         if (rand < 50)
         {
+            _state = State.Default;
             // 停止 or 旋回処理
 
         }
         else
         {
+            _state = State.Move;
             // プレイヤーに近寄る
             _chase = true;
         }
@@ -133,8 +160,18 @@ public class Enemy : MonoBehaviour
         _previousPosition = transform.position;
         var nextPosition = _previousPosition;
 
-        _acceleration = ZERO;
+        if (_state == State.Knockback)
+        {
+            var knockBackRight = transform.position.x > _targetTransform.position.x;
+            _speed = knockBackRight ? KNOCK_BACK_SPEED : -KNOCK_BACK_SPEED;
 
+            _state = State.Damaging;
+            _remainMoveTime = .5f;
+        }
+        else
+        {
+            _acceleration = ZERO;
+        }
 
         // 重力加速度の適用
         var isAppryGravity = !_isGround;
@@ -146,6 +183,14 @@ public class Enemy : MonoBehaviour
         {
             // 重力を適用しないときはそもそも下に落ちないようにする
             _speed.y = 0;
+        }
+
+        // ダメージを受けてる間は横のスピードを経過時間で制御
+        if (_state == State.Damaging)
+        {
+            var temp = _speed;
+            temp.x = 0.9f * temp.x;
+            _speed = temp;
         }
 
         // 物理演算による移動
@@ -191,6 +236,7 @@ public class Enemy : MonoBehaviour
     public void OnDamage(int hitCount, int amount)
     {
         _hp -= amount * hitCount;
+
         for (int i = 0; i < hitCount; i++)
         {
             var damageText = GameObject.Instantiate<DamageText>(_damageText, transform);
@@ -204,6 +250,11 @@ public class Enemy : MonoBehaviour
         {
 
             _destroyAnimation.Play();
+            _state = State.Damaging; // 死ぬ場合はノックバックしない
+        }
+        else
+        {
+            _state = State.Knockback;
         }
     }
     
